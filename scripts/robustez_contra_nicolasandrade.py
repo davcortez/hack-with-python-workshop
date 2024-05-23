954a139eb9632eb8d39577fd73ea4a2bfbe2f65b
nicoandrade50 <83848730+nicoandrade50@users.noreply.github.com>
2024-05-23 12:24:51 -0500

    Create robustez_contra_nicolasandrade.py

---
+++ scripts/robustez_contra_nicolasandrade.py
@@ -0,0 +1,33 @@
+def verificar_robustez_contraseña(contraseña):
+
+    longitud_suficiente = len(contraseña) >= 8
+    
+
+    contiene_mayusculas = any(c.isupper() for c in contraseña)
+    
+
+    contiene_minusculas = any(c.islower() for c in contraseña)
+    
+
+    contiene_digitos = any(c.isdigit() for c in contraseña)
+    
+
+    contiene_caracteres_especiales = any(c in "!@#$%^&*()-_=+[]{}|;:',.<>?/" for c in contraseña)
+    
+
+    puntuacion = longitud_suficiente + contiene_mayusculas + contiene_minusculas + contiene_digitos + contiene_caracteres_especiales
+    
+
+    if puntuacion >= 5:
+        print("¡La contraseña es robusta!")
+    else:
+        print("La contraseña no es lo suficientemente robusta.")
+
+
+    print("Puntuación de la contraseña:", puntuacion)
+
+
+contraseña = input("Introduce tu contraseña: ")
+
+
+verificar_robustez_contraseña(contraseña)
